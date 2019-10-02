54fc2487ebce850b514b1bb50b13c0aa6a38a775
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-10-02 17:48:11 +0900

    Connect to postgres

--- handler.ts
+++ handler.ts
@@ -1,6 +1,16 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
+import { Client } from "pg";
 
 export const hello: APIGatewayProxyHandler = async event => {
+  const pgClient = new Client({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  });
+  await pgClient.connect();
+  const res = await pgClient.query('SELECT * FROM "users"');
+  console.info("res", res);
   return {
     statusCode: 200,
     body: JSON.stringify({
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const webpack = require('webpack');
 
 const entries = {};
 
@@ -12,18 +13,19 @@ module.exports = {
   entry: entries,
   devtool: 'source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
   },
+  plugins: [new webpack.IgnorePlugin(/^pg-native$/)],
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
+    filename: '[name].js'
   },
   target: 'node',
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-    ],
-  },
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
 };
